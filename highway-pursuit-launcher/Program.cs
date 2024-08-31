b8f698d329f4ef7dfdf6d66ecef0204d463db4c1
Nils Ruet <nilsruet@gmx.fr>
2024-08-31 20:54:09 +0200

    init

---
+++ highway-pursuit-launcher/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayPursuitLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string channelName = null;
+
+            ProcessArgs(args, out string targetExe);
+
+            if (string.IsNullOrEmpty(targetExe)) return;
+
+            // Create the IPC server using the FileMonitorIPC.ServiceInterface class as a singleton
+            EasyHook.RemoteHooking.IpcCreateServer<HighwayPursuitServer.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+
+            string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "HighwayPursuitServer.dll");
+
+            try
+            {
+                Console.WriteLine("Attempting to create and inject into {0}", targetExe);
+                // start and inject into a new process
+                EasyHook.RemoteHooking.CreateAndInject(
+                    targetExe,
+                    "",
+                    0,
+                    EasyHook.InjectionOptions.DoNotRequireStrongName,
+                    injectionLibrary,
+                    injectionLibrary,
+                    out _,
+                    channelName
+                );
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There was an error while injecting into target:");
+                Console.ResetColor();
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("Success!");
+            Console.WriteLine("<Press any key to exit>");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        static void ProcessArgs(string[] args, out string targetExe)
+        {
+            targetExe = null;
+            Console.WriteLine("Starting Highway pursuit...");
+
+            // Load any parameters
+            if (args.Length != 1 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: HighwayPursuitLauncher PathToExecutable");
+            }
+            else
+            {
+                targetExe = args[0];
+            }
+        }
+    }
+}
---
+++ highway-pursuit-server/ServerInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayPursuitServer
+{
+    public class ServerInterface : MarshalByRefObject
+    {
+        public void IsInstalled(int clientPID)
+        {
+            Console.WriteLine("FileMonitor has injected FileMonitorHook into process {0}.\r\n", clientPID);
+        }
+
+        public void ReportMessages(string[] messages)
+        {
+            for (int i = 0; i < messages.Length; i++)
+            {
+                Console.WriteLine(messages[i]);
+            }
+        }
+
+        public void ReportMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ReportException(Exception e)
+        {
+            Console.WriteLine("The target process has reported an error:\r\n" + e.ToString());
+        }
+
+        int count = 0;
+        public void Ping()
+        {
+            var oldTop = Console.CursorTop;
+            var oldLeft = Console.CursorLeft;
+            Console.CursorVisible = false;
+
+            var chars = "\\|/-";
+            Console.SetCursorPosition(Console.WindowWidth - 1, oldTop - 1);
+            Console.Write(chars[count++ % chars.Length]);
+
+            Console.SetCursorPosition(oldLeft, oldTop);
+            Console.CursorVisible = true;
+        }
+    }
+}
