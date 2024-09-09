c906d9d471313fcb119637de37aeb79dc25e10ed
Nils Ruet <nilsruet@gmx.fr>
2024-09-09 13:30:34 +0200

    tmp: client-side reset instruction

--- highway-pursuit-gym/envs/_remote/highway_pursuit_client.py
+++ highway-pursuit-gym/envs/_remote/highway_pursuit_client.py
@@ -49,7 +49,7 @@ class HighwayPursuitClient:
         """
         # Generate name for mutex and share memory sections
 
-        self.app_resources_id = f"{str(uuid.uuid1())}-"
+        self._app_resources_id = f"{str(uuid.uuid1())}-"
         # Setup the server and the data formats
         self.setup_server()
         return self.observation_shape, self.action_count
@@ -61,7 +61,7 @@ class HighwayPursuitClient:
             self.highway_pursuit_path,
             self.dll_path,
             str(self.is_real_time),
-            self.app_resources_id
+            self._app_resources_id
         ]
 
         # Run the command
@@ -73,61 +73,65 @@ class HighwayPursuitClient:
 
     def setup_server(self):
         # Define shared resources names
-        lock_server_name = f"{self.app_resources_id}{server_mutex_id}"
-        lock_client_name = f"{self.app_resources_id}{client_mutex_id}"
+        lock_server_name = f"{self._app_resources_id}{server_mutex_id}"
+        lock_client_name = f"{self._app_resources_id}{client_mutex_id}"
 
-        server_info_memory_name = f"{self.app_resources_id}{server_info_memory_id}"
-        instruction_memory_name = f"{self.app_resources_id}{instruction_memory_id}"
-        observation_memory_name = f"{self.app_resources_id}{observation_memory_id}"
-        info_memory_name = f"{self.app_resources_id}{info_memory_id}"
-        reward_memory_name = f"{self.app_resources_id}{reward_memory_id}" 
-        action_memory_name = f"{self.app_resources_id}{action_memory_id}" 
+        server_info_memory_name = f"{self._app_resources_id}{server_info_memory_id}"
+        instruction_memory_name = f"{self._app_resources_id}{instruction_memory_id}"
+        observation_memory_name = f"{self._app_resources_id}{observation_memory_id}"
+        info_memory_name = f"{self._app_resources_id}{info_memory_id}"
+        reward_memory_name = f"{self._app_resources_id}{reward_memory_id}" 
+        action_memory_name = f"{self._app_resources_id}{action_memory_id}" 
 
         # Create semaphores for synchronization
         # Initially no availability
-        self.lock_server_pool = Semaphore(lock_server_name, initial_count=0, max_count=1) 
-        self.lock_client_pool = Semaphore(lock_client_name, initial_count=0, max_count=1)
+        self._lock_server_pool = Semaphore(lock_server_name, initial_count=0, max_count=1) 
+        self._lock_client_pool = Semaphore(lock_client_name, initial_count=0, max_count=1)
 
         # Create the initial shared memory for retrieving server info
-        self.server_info_sm = shared_memory.SharedMemory(name=server_info_memory_name, size=ctypes.sizeof(ServerInfo), create=True)
+        self._server_info_sm = shared_memory.SharedMemory(name=server_info_memory_name, size=ctypes.sizeof(ServerInfo), create=True)
         
         # Start the server process
         self._start_process()
 
         # Notify server that server info is ready to be retrieved, and wait for the operation to complete
-        self.lock_server_pool.release()
-        self.lock_client_pool.acquire()
+        self._lock_server_pool.release()
+        self._lock_client_pool.acquire()
 
         # Retrieve the server info
-        server_info: ServerInfo = ServerInfo.from_buffer_copy(self.server_info_sm.buf)
+        server_info: ServerInfo = ServerInfo.from_buffer_copy(self._server_info_sm.buf)
         self.observation_shape = (server_info.obs_width, server_info.obs_height, server_info.obs_channels)
         self.action_count = server_info.action_count
         
         # Create the remaining shared memory
-        self.instruction_sm = shared_memory.SharedMemory(name=instruction_memory_name, size=ctypes.sizeof(Instruction), create=True)
-        self.info_sm = shared_memory.SharedMemory(name=info_memory_name, size=ctypes.sizeof(Info), create=True)
-        self.reward_sm = shared_memory.SharedMemory(name=reward_memory_name, size=ctypes.sizeof(Reward), create=True)
+        self._instruction_sm = shared_memory.SharedMemory(name=instruction_memory_name, size=ctypes.sizeof(Instruction), create=True)
+        self._info_sm = shared_memory.SharedMemory(name=info_memory_name, size=ctypes.sizeof(Info), create=True)
+        self._reward_sm = shared_memory.SharedMemory(name=reward_memory_name, size=ctypes.sizeof(Reward), create=True)
 
         observation_buffer_size = np.prod(self.observation_shape).item()
         action_buffer_size = self.action_count # one byte per action
-        self.observation_sm = shared_memory.SharedMemory(name=observation_memory_name, size=observation_buffer_size, create=True)
-        self.action_sm = shared_memory.SharedMemory(name=action_memory_name, size=action_buffer_size, create=True)
+        self._observation_sm = shared_memory.SharedMemory(name=observation_memory_name, size=observation_buffer_size, create=True)
+        self._action_sm = shared_memory.SharedMemory(name=action_memory_name, size=action_buffer_size, create=True)
 
         # Release the server, it will connect to the shared memory sections
-        self.lock_server_pool.release()
+        self._lock_server_pool.release()
 
         # Lock the client mutex to ensure the server is in wait mode
-        self.lock_client_pool.acquire()
+        self._lock_client_pool.acquire()
 
     def _write_instruction_unsafe(self, instruction: Instruction):
         """This writes an instruction in the appropriate shared memory, without synchronization"""
         bytes = bytearray(instruction)
-        self.instruction_sm.buf[:len(bytes)] = bytes
+        self._instruction_sm.buf[:len(bytes)] = bytes
 
     def reset(self):
+        # Query
         self._sync_instruction(Instruction(Instruction.RESET))
-        # state, info
-        return None, None
+        # Result
+        info: Info = Info.from_buffer_copy(self._info_sm.buf)
+        observation = np.ndarray(self.observation_shape, dtype=np.uint8, buffer=self._observation_sm.buf)
+
+        return observation, info
 
     def step(self, action):
         self._sync_instruction(Instruction(Instruction.STEP))
@@ -140,24 +144,24 @@ class HighwayPursuitClient:
 
         # At this point, the server shouldn't use any shared resource
         # Clean up everything
-        self.server_info_sm.close()
-        self.instruction_sm.close()
-        self.observation_sm.close()
-        self.info_sm.close()
-        self.reward_sm.close()
-        self.action_sm.close()
+        self._server_info_sm.close()
+        self._instruction_sm.close()
+        self._observation_sm.close()
+        self._info_sm.close()
+        self._reward_sm.close()
+        self._action_sm.close()
 
-        self.server_info_sm.unlink()
-        self.instruction_sm.unlink()
-        self.observation_sm.unlink()
-        self.info_sm.unlink()
-        self.reward_sm.unlink()
-        self.action_sm.unlink()
+        self._server_info_sm.unlink()
+        self._instruction_sm.unlink()
+        self._observation_sm.unlink()
+        self._info_sm.unlink()
+        self._reward_sm.unlink()
+        self._action_sm.unlink()
 
-        self.lock_client_pool.close()
-        self.lock_server_pool.close()
+        self._lock_client_pool.close()
+        self._lock_server_pool.close()
 
     def _sync_instruction(self, instruction):
         self._write_instruction_unsafe(instruction)
-        self.lock_server_pool.release()
-        self.lock_client_pool.acquire()
+        self._lock_server_pool.release()
+        self._lock_client_pool.acquire()
